57a4ae80b55ab0438d6ded1642c5ab44e2046874
Ryan O'Hara <preciselytwentysevenletters@outlook.com>
2012-11-04 17:52:01 -0800

    Made it "always on top"; added animations.
    It was already TopMost, but that doesn't work when you Show Desktop.
    Also, turned Option Strict On. (I'm not sure why it wasn't already on.)

---
+++ Typistry/Extensions.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Runtime.CompilerServices
+
+Module Extensions
+    Public Function EaseInOut(value As Double) As Double
+        If value < 0 OrElse value > 1 Then Throw New ArgumentException("Value must be between 0 and 1 (inclusive).", "value")
+
+        Return Math.Sin(value * Math.PI / 2)
+    End Function
+
+    ''' <summary>
+    ''' Performs an animation on a <see cref="Control" />.
+    ''' </summary>
+    ''' <param name="this">The control performing the animation.</param>
+    ''' <param name="callback">The animation callback.</param>
+    ''' <param name="duration">The duration of the animation in seconds.</param>
+    <Extension()>
+    Public Function Animate(this As Control, callback As Action(Of Double), duration As Double) As Threading.Timer
+        Dim start As Date = Date.Now
+
+        Animate = New Threading.Timer(Sub()
+                                          Dim time As Double = Date.Now.Subtract(start).TotalSeconds / duration
+
+                                          If time > 1 Then
+                                              time = 1
+                                              Animate.Dispose()
+                                          End If
+
+                                          this.Invoke(callback, time)
+                                      End Sub, Nothing, 0, 10)
+    End Function
+End Module
--- Typistry/Prompt.vb
+++ Typistry/Prompt.vb
@@ -4,11 +4,19 @@ Imports System.Reflection
 Public Class Prompt
     Private hook As IntPtr
     Private hookDelegate As New KBDLLHookProc(AddressOf KeyHook)
+    Private isVisible As Boolean = False
+    Private currentAnimation As Threading.Timer
+
+    <DllImport("user32.dll")>
+    Private Shared Function SetForegroundWindow(hWnd As IntPtr) As Integer
+
+    End Function
 
     Private Sub Prompt_Load(sender As Object, e As EventArgs) Handles Me.Load
         Dim workingArea As Rectangle = My.Computer.Screen.WorkingArea
 
-        Me.Bounds = New Rectangle(workingArea.X, workingArea.Y, workingArea.Width, Me.Height)
+        Me.Bounds = New Rectangle(workingArea.X, workingArea.Y, workingArea.Width, 1)
+        Me.Opacity = 0
 
         Dim hookAssembly As Assembly
 
@@ -34,6 +42,31 @@ Public Class Prompt
                 Dim keyCode As Keys = CType(CType(Marshal.PtrToStructure(lParam, GetType(KeyboardHook.KBDLLHOOKSTRUCT)), KeyboardHook.KBDLLHOOKSTRUCT).vkCode, Keys)
 
                 If keyCode = Keys.CapsLock Then
+                    isVisible = Not isVisible
+
+                    If currentAnimation IsNot Nothing Then currentAnimation.Dispose()
+
+                    Dim currentHeight As Integer = Me.Height
+                    Dim currentOpacity As Double = Me.Opacity
+
+                    If isVisible Then
+                        currentAnimation = Me.Animate(Sub(t)
+                                                          t = EaseInOut(t)
+
+                                                          Me.Opacity = currentOpacity + (1 - currentOpacity) * t
+                                                          Me.Height = CInt(currentHeight + (40 - currentHeight) * t)
+                                                      End Sub, 0.2)
+
+                        SetForegroundWindow(Me.Handle)
+                    Else
+                        currentAnimation = Me.Animate(Sub(t)
+                                                          t = EaseInOut(t)
+
+                                                          Me.Opacity = currentOpacity * (1 - t)
+                                                          Me.Height = CInt(currentHeight * (1 - t))
+                                                      End Sub, 0.2)
+                    End If
+
                     ' Finally, prevent Caps Lock from being turned on:
                     Return 1
                 End If
@@ -43,15 +76,19 @@ Public Class Prompt
         Return KeyboardHook.CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam)
     End Function
 
-    Protected Overrides Sub Finalize()
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+
             If hook <> IntPtr.Zero Then
                 Dim success As Boolean = KeyboardHook.UnhookWindowsHookEx(hook)
 
                 Debug.Assert(success)
             End If
         Finally
-            MyBase.Finalize()
+            MyBase.Dispose(disposing)
         End Try
     End Sub
 End Class
