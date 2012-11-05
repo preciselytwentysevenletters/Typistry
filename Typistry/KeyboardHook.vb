401302988a06f54c5b7c61b36c1f2f9585a8a379
Ryan O'Hara <preciselytwentysevenletters@outlook.com>
2012-11-04 17:12:28 -0800

    First commit with the skeleton for the keyhook.
    A bit of the general layout and colour, too.

---
+++ Typistry/KeyboardHook.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Module KeyboardHook
+    <DllImport("user32.dll")>
+    Public Function SetWindowsHookEx(idHook As Integer, HookProc As KBDLLHookProc, hInstance As IntPtr, dwThreadId As UShort) As IntPtr
+
+    End Function
+
+    <DllImport("user32.dll")>
+    Public Function CallNextHookEx(idHook As IntPtr, nCode As Integer, wParam As IntPtr, lParam As IntPtr) As Integer
+
+    End Function
+
+    <DllImport("user32.dll")>
+    Public Function UnhookWindowsHookEx(idHook As IntPtr) As Boolean
+
+    End Function
+
+    <StructLayout(LayoutKind.Sequential)>
+    Public Structure KBDLLHOOKSTRUCT
+        Public vkCode As UInteger
+        Public scanCode As UInteger
+        Public flags As KBDLLHOOKSTRUCTFlags
+        Public time As UInteger
+        Public dwExtraInfo As UIntPtr
+    End Structure
+
+    <Flags()>
+    Public Enum KBDLLHOOKSTRUCTFlags As UInteger
+        LLKHF_EXTENDED = &H1
+        LLKHF_INJECTED = &H10
+        LLKHF_ALTDOWN = &H20
+        LLKHF_UP = &H80
+    End Enum
+
+    Public Const WH_KEYBOARD As Integer = 2
+    Public Const WH_KEYBOARD_LL As Integer = 13
+    Public Const HC_ACTION As Integer = 0
+    Public Const WM_KEYDOWN As Integer = &H100
+    Public Const WM_KEYUP As Integer = &H101
+    Public Const WM_SYSKEYDOWN As Integer = &H104
+    Public Const WM_SYSKEYUP As Integer = &H105
+
+    Public Delegate Function KBDLLHookProc(nCode As Integer, wParam As IntPtr, lParam As IntPtr) As Integer
+End Module
---
+++ Typistry/Prompt.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Reflection
+
+Public Class Prompt
+    Private hook As IntPtr
+    Private hookDelegate As New KBDLLHookProc(AddressOf KeyHook)
+
+    Private Sub Prompt_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim workingArea As Rectangle = My.Computer.Screen.WorkingArea
+
+        Me.Bounds = New Rectangle(workingArea.X, workingArea.Y, workingArea.Width, Me.Height)
+
+        Dim hookAssembly As Assembly
+
+        ' The Visual Studio hosting process interferes with this =(
+#If DEBUG Then
+        hookAssembly = Assembly.GetCallingAssembly()
+#Else
+        hookAssembly = Assembly.GetExecutingAssembly()
+#End If
+
+        hook = KeyboardHook.SetWindowsHookEx(KeyboardHook.WH_KEYBOARD_LL, hookDelegate, Marshal.GetHINSTANCE(hookAssembly.Modules.First()), 0)
+
+        If hook = IntPtr.Zero Then
+            Throw New ApplicationException("Failed to set key hook.")
+        End If
+    End Sub
+
+    Private Function KeyHook(nCode As Integer, wParam As IntPtr, lParam As IntPtr) As Integer
+        If nCode = KeyboardHook.HC_ACTION Then
+            Dim p As Integer = wParam.ToInt32()
+
+            If p = KeyboardHook.WM_KEYDOWN OrElse p = KeyboardHook.WM_SYSKEYDOWN Then
+                Dim keyCode As Keys = CType(CType(Marshal.PtrToStructure(lParam, GetType(KeyboardHook.KBDLLHOOKSTRUCT)), KeyboardHook.KBDLLHOOKSTRUCT).vkCode, Keys)
+
+                If keyCode = Keys.CapsLock Then
+                    ' Finally, prevent Caps Lock from being turned on:
+                    Return 1
+                End If
+            End If
+        End If
+
+        Return KeyboardHook.CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam)
+    End Function
+
+    Protected Overrides Sub Finalize()
+        Try
+            If hook <> IntPtr.Zero Then
+                Dim success As Boolean = KeyboardHook.UnhookWindowsHookEx(hook)
+
+                Debug.Assert(success)
+            End If
+        Finally
+            MyBase.Finalize()
+        End Try
+    End Sub
+End Class
